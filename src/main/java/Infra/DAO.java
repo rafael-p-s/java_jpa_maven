586def60d3dcb6cd401b941b88c1e631d291dff7
rafael-p-s <rafaelsky321@gmail.com>
2026-08-12 18:01:41 -0300

    Criado o DAO

---
+++ src/main/java/Infra/DAO.java
@@ -0,0 +1,84 @@
+package Infra;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class DAO<E> {
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+    private Class<E> classe;
+
+    // Quando a classe for carregada ele irá carregar aqui primeiro, necessário cuidado.
+    static {
+        try {
+            emf = Persistence
+                    .createEntityManagerFactory("udemy_maven");
+        } catch (Exception e) {
+            // logar -> log4j -> api para ajudar a criar logs de inicialização
+        }
+    }
+
+    public DAO() {
+        this(null);
+    }
+
+    public DAO(Class<E> classe) {
+        this.classe = classe;
+        em = emf.createEntityManager();
+    }
+
+    /*
+    Retorna a própria classe.
+    public DAO<E> teste() {
+        return this;
+    }*/
+
+    public DAO<E> abrirTransaction() {
+        em.getTransaction().begin();
+        return this;
+    }
+
+    public DAO<E> fecharTransaction() {
+        em.getTransaction().commit();
+        return this;
+    }
+
+    public DAO<E> incluir(E entidade) {
+        em.persist(entidade);
+        return this;
+    }
+
+    // Incluir Geral
+    public DAO<E> incluirAtomico(E entidade) {
+        return this.abrirTransaction().incluir(entidade).fecharTransaction();
+    }
+
+    // Vai servir para buscar informações no banco, porém exibir com limite na tela
+    public List<E> obterTodosRegistros(int qtde, int deslocamento) {
+        if (classe == null) {
+            throw new UnsupportedOperationException("Null Class");
+        }
+
+        String jpql = "select e from " + classe.getName() + " e"; // sempre precisa desse "e" no final
+        TypedQuery<E> query = em.createQuery(jpql, classe);
+        query.setMaxResults(qtde);
+        query.setFirstResult(deslocamento);
+        return query.getResultList();
+    }
+
+    // Caso eu queria passar tudo, sem tratamento de tamanho a ser passado
+    public List<E> ObterTodos() {
+        return this.obterTodosRegistros(10, 0);
+    }
+
+    // Aqui só fechar o Manager
+    // Não será fechado o EntityManagerFactory pois ele está como static
+    public void fechar() {
+        em.close();
+    }
+
+}
--- src/main/java/modelo/basico/produto/Produto.java
+++ src/main/java/modelo/basico/produto/Produto.java
@@ -1,5 +1,51 @@
 package modelo.basico.produto;
 
-public class Produto {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tb_produtos")
+// schema = "curso_java" ->schema serve para informar o banco que vai usar isso. Não e´muito usado
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 200, nullable = false)
+    private String nome;
+    @Column(nullable = false, scale = 2)
+    private Double preco;
+
+    // Construtor Padrão
+    public Produto() {
+
+    }
+
+    public Produto(String nome, Double preco) {
+        super();
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
